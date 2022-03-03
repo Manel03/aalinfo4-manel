f885c8a49ec329c08a5da186ba1bb312e532fbe8
manel.hamrouni@esprit.tn <manel.hamrouni@esprit.tn>
2022-03-03 12:50:04 +0100

    added

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -7,6 +7,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 
@@ -18,7 +19,7 @@ public class CallRestWebService {
 
 	public static final String endpoint = "http://ip-api.com/json";
 	//public static final String endpoint = "https://httpbin.org/get";
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JSONException {
 		HttpClient client = new DefaultHttpClient();
 		HttpGet request = new HttpGet(endpoint);
 		String ip = "not found";
@@ -35,6 +36,8 @@ public class CallRestWebService {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+                
+                System.out.println("Project de coaching GIT");
 	}
 }
 
